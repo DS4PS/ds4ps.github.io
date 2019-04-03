a73ed27685434f719f5a00507ff3882b7756334e
Jesse Lecy <jdlecy@gmail.com>
2019-04-02 20:08:42 -0700

    add screencasts

---
+++ gifs/NewRmarkdownFile/NewRmarkdownFile_media/NewRmarkdownFile_player_embed.js
@@ -0,0 +1,2 @@
+var oeTags = '<img src="NewRmarkdownFile_media/NewRmarkdownFile.gif" width="1180" height="888" alt=""/>';         
+document.write( oeTags );
---
+++ gifs/html_format/html_format_media/html_format_player_embed.js
@@ -0,0 +1,2 @@
+var oeTags = '<img src="html_format_media/html_format.gif" width="1180" height="888" alt=""/>';         
+document.write( oeTags );
